d131c8decca2ccfd172267eaa51be532aa9e61a9
Borneol <a1991818@gmail.com>
2025-06-24 14:57:32 +0800

    分支後完成初始設定並建立API控制器

---
+++ BorneolTestOfMERCURYFIRE/Controllers/Myoffice_ACPDapiController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Text.Json;
+using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace BorneolTestOfMERCURYFIRE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Myoffice_ACPDapiController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+        private readonly string _connStr;
+        public Myoffice_ACPDapiController(IConfiguration config)
+        {
+            _config = config;
+            _connStr = _config.GetConnectionString("MERCURYFIRE");
+        }
+        //查詢表中資料
+        [HttpGet("GetAllMyoffice_ACPDs")]
+        public async Task<IActionResult> GetAllMyoffice_ACPDs()
+        {
+            using var conStr = new SqlConnection(_connStr);
+            var result = await conStr.QuerySingleAsync<string>("usp_GetAllMyoffice_ACPDs", 
+                commandType: CommandType.StoredProcedure);
+            if (string.IsNullOrEmpty(result))
+            {
+                return NotFound("No data found.");
+            }
+            return Ok(result);
+        }
+        //新增一筆資料
+        [HttpPost("CreateMyoffice_ACPD")]
+        public async Task<IActionResult> CreateMyoffice_ACPD([FromBody] JsonElement json)
+        {
+            using var conStr = new SqlConnection(_connStr);
+            var jsonData=json.GetRawText();
+
+            await conStr.ExecuteAsync("usp_CreateMyoffice_ACPD",
+                new { Json=jsonData },
+                commandType: CommandType.StoredProcedure);
+
+            return Ok("新增成功");
+        }
+        //更新一筆資料
+        [HttpPut("UpdateMyoffice_ACPD")]
+        public async Task<IActionResult> UpdateMyoffice_ACPD([FromBody] JsonElement json)
+        {
+            using var conStr = new SqlConnection(_connStr);
+            var jsonData = json.GetRawText();
+
+            await conStr.ExecuteAsync("usp_UpdateMyoffice_ACPD",
+                new { Json = jsonData },
+                commandType: CommandType.StoredProcedure);
+
+            return Ok("更新成功");
+        }
+        //刪除一筆資料
+        [HttpDelete("DeleteMyoffice_ACPD")]
+        public async Task<IActionResult> DeleteMyoffice_ACPD([FromBody] JsonElement json)
+        {
+            using var conStr = new SqlConnection(_connStr);
+            var jsonData = json.GetRawText();
+
+            await conStr.ExecuteAsync("usp_DeleteMyoffice_ACPD",
+                new { Json = jsonData },
+                commandType: CommandType.StoredProcedure);
+
+            return Ok("刪除成功");
+        }
+    }
+}

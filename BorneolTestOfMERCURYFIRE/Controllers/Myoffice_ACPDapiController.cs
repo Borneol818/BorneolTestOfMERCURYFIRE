3e14dc5386b5df88fbc199ef7c90065b2d2ede78
Borneol <a1991818@gmail.com>
2025-06-24 16:00:53 +0800

    最後放入.bak在DataBase

--- BorneolTestOfMERCURYFIRE/Controllers/Myoffice_ACPDapiController.cs
+++ BorneolTestOfMERCURYFIRE/Controllers/Myoffice_ACPDapiController.cs
@@ -23,7 +23,7 @@ namespace BorneolTestOfMERCURYFIRE.Controllers
         public async Task<IActionResult> GetAllMyoffice_ACPDs()
         {
             using var conStr = new SqlConnection(_connStr);
-            var result = await conStr.QuerySingleAsync<string>("usp_GetAllMyoffice_ACPDs", 
+            var result = await conStr.QuerySingleAsync<string>("usp_GetAllMyoffice_ACPDs",
                 commandType: CommandType.StoredProcedure);
             if (string.IsNullOrEmpty(result))
             {
@@ -32,43 +32,61 @@ namespace BorneolTestOfMERCURYFIRE.Controllers
             return Ok(result);
         }
         //新增一筆資料
-        [HttpPost("CreateMyoffice_ACPD")]
-        public async Task<IActionResult> CreateMyoffice_ACPD([FromBody] JsonElement json)
-        {
-            using var conStr = new SqlConnection(_connStr);
-            var jsonData=json.GetRawText();
+        //JSON格式範例：
+        //{
+        //    "ACPD_SID":"0",
+        //    "ACPD_Cname":"測試001",
+        //    "ACPD_Ename":"test001",
+        //    "ACPD_Sname":"test001",
+        //    "ACPD_Email":"test001@test001.com",
+        //    "ACPD_Status":"test001",
+        //    "ACPD_Stop":"test001",
+        //    "ACPD_StopMemo":"test001",
+        //    "ACPD_LoginID":"0",
+        //    "ACPD_LoginPWD":"test001",
+        //    "ACPD_Memo":"test001",
+        //    "ACPD_NowDateTime":"2025-06-24 00:00:00",
+        //    "ACPD_NowID":"0",
+        //    "ACPD_UPDDateTime":"2025-06-24 00:00:00",
+        //    "ACPD_UPDID)":"0"
+        //}
+    [HttpPost("CreateMyoffice_ACPD")]
+    public async Task<IActionResult> CreateMyoffice_ACPD([FromBody] JsonElement json)
+    {
+        using var conStr = new SqlConnection(_connStr);
+        var jsonData = json.GetRawText();
 
-            await conStr.ExecuteAsync("usp_CreateMyoffice_ACPD",
-                new { Json=jsonData },
-                commandType: CommandType.StoredProcedure);
+        await conStr.ExecuteAsync("usp_CreateMyoffice_ACPD",
+            new { Json = jsonData },
+            commandType: CommandType.StoredProcedure);
 
-            return Ok("新增成功");
-        }
-        //更新一筆資料
-        [HttpPut("UpdateMyoffice_ACPD")]
-        public async Task<IActionResult> UpdateMyoffice_ACPD([FromBody] JsonElement json)
-        {
-            using var conStr = new SqlConnection(_connStr);
-            var jsonData = json.GetRawText();
-
-            await conStr.ExecuteAsync("usp_UpdateMyoffice_ACPD",
-                new { Json = jsonData },
-                commandType: CommandType.StoredProcedure);
-
-            return Ok("更新成功");
-        }
-        //刪除一筆資料
-        [HttpDelete("DeleteMyoffice_ACPD")]
-        public async Task<IActionResult> DeleteMyoffice_ACPD([FromBody] JsonElement json)
-        {
-            using var conStr = new SqlConnection(_connStr);
-            var jsonData = json.GetRawText();
-
-            await conStr.ExecuteAsync("usp_DeleteMyoffice_ACPD",
-                new { Json = jsonData },
-                commandType: CommandType.StoredProcedure);
-
-            return Ok("刪除成功");
-        }
+        return Ok("新增成功");
     }
+    //更新一筆資料
+    [HttpPut("UpdateMyoffice_ACPD")]
+    public async Task<IActionResult> UpdateMyoffice_ACPD([FromBody] JsonElement json)
+    {
+        using var conStr = new SqlConnection(_connStr);
+        var jsonData = json.GetRawText();
+
+        await conStr.ExecuteAsync("usp_UpdateMyoffice_ACPD",
+            new { Json = jsonData },
+            commandType: CommandType.StoredProcedure);
+
+        return Ok("更新成功");
+    }
+    //刪除一筆資料
+    [HttpDelete("DeleteMyoffice_ACPD")]
+    public async Task<IActionResult> DeleteMyoffice_ACPD([FromBody] JsonElement json)
+    {
+        using var conStr = new SqlConnection(_connStr);
+        var jsonData = json.GetRawText();
+
+        await conStr.ExecuteAsync("usp_DeleteMyoffice_ACPD",
+            new { Json = jsonData },
+            commandType: CommandType.StoredProcedure);
+
+        return Ok("刪除成功");
+    }
+}
 }
